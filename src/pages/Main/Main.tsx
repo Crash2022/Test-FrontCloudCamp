b35513889ff704773f3b7c76e3a26af0650636f3
Crash2022 <crash555@mail.ru>
2023-06-07 20:26:09 +0500

    add: Main header styles

--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -1,11 +1,24 @@
-import React from 'react';
+import React from 'react'
 import s from './Main.module.scss'
-import {Avatar} from '../../shared/ui/Avatar/Avatar';
+import {Avatar} from '../../shared/ui/Avatar/Avatar'
+import FolderIcon from '../../shared/assets/icons/folder-icon.svg'
+
+type ContactsType = {
+    id: number
+    title: string
+    link: string
+}
 
 export const Main = () => {
 
     const lastName: string = 'Чашин'
     const firstName: string = 'Александр'
+
+    const contacts: ContactsType[] = [
+        {id: 1, title: 'Telegram', link: 'https://t.me/MyNameCrash'},
+        {id: 2, title: 'GitHub', link: 'https://github.com/Crash2022'},
+        {id: 3, title: 'Resume', link: 'https://my-portfolio-app-beryl.vercel.app/'},
+    ]
 
     return (
         <div className={s.main_mainBox}>
@@ -17,7 +30,20 @@ export const Main = () => {
                             {lastName} {firstName}
                         </div>
                         <div className={s.header_infoContacts}>
-                            3
+                            {
+                                contacts.map(c => {
+                                    return (
+                                        <div key={c.id} className={s.contactItem}>
+                                            <div className={s.contactItem_icon}>
+                                                <img src={FolderIcon} alt='folder-icon' />
+                                            </div>
+                                            <div className={s.contactItem_link}>
+                                                <a href={c.link} target='_blank'>{c.title}</a>
+                                            </div>
+                                        </div>
+                                    )
+                                })
+                            }
                         </div>
                     </div>
                 </div>
