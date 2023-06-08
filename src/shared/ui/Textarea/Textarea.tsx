2a9b0f97b5fd4192c1aec88663b828a2ccc710b8
Crash2022 <crash555@mail.ru>
2023-06-08 14:44:45 +0500

    add: Textarea

---
+++ src/shared/ui/Textarea/Textarea.tsx
@@ -0,0 +1,56 @@
+import React, {
+    ChangeEvent,
+    DetailedHTMLProps,
+    InputHTMLAttributes,
+    KeyboardEvent,
+    ReactNode
+} from 'react'
+import s from './Textarea.module.scss'
+
+type DefaultInputPropsType = DetailedHTMLProps<
+    InputHTMLAttributes<HTMLTextAreaElement>,
+    HTMLTextAreaElement
+>
+
+type CustomTextareaPropsType = DefaultInputPropsType & {
+    onChangeText?: (value: string) => void
+    onEnter?: () => void
+    error?: ReactNode
+    placeholder?: string
+    helperText?: string
+    divClassName?: string
+    spanClassName?: string
+}
+
+export const Textarea: React.FC<CustomTextareaPropsType> = ({
+    // onChange,
+    onChangeText,
+    onKeyPress,
+    onEnter,
+    error,
+    placeholder,
+    helperText, // error для Controlled InputUI (React Hook Forms)
+    className,
+    divClassName,
+    ...restProps // все остальные пропсы попадут в объект restProps
+}) => {
+    const onChangeCallback = (e: ChangeEvent<HTMLTextAreaElement>) => {
+        // onChange && onChange(e);
+        onChangeText && onChangeText(e.currentTarget.value)
+    }
+    const onKeyPressCallback = (e: KeyboardEvent<HTMLTextAreaElement>) => {
+        onKeyPress && onKeyPress(e)
+        onEnter && e.key === 'Enter' && onEnter()
+    }
+
+    const finalTextareaDivWrapperClassName = `${divClassName || s.textarea_wrapper}`
+
+    return (
+        <div className={finalTextareaDivWrapperClassName}>
+            <div className={s.placeholder}>{placeholder}</div>
+            {/* {error && <span className={s.errorMessage}>{error}</span>} */}
+            <textarea onChange={onChangeCallback} onKeyPress={onKeyPressCallback} {...restProps} />
+            {error && <span className={s.errorMessage}>{error}</span>}
+        </div>
+    )
+}
