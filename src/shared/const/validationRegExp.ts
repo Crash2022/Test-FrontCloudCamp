a0fcde6f246fe51d2c4c2d4f819c77dc520cf057
Crash2022 <crash555@mail.ru>
2023-06-12 22:51:30 +0500

    refactor: code

--- src/shared/const/validationRegExp.ts
+++ src/shared/const/validationRegExp.ts
@@ -1,6 +1,6 @@
-export const phoneRegExp = /^\+7((\\+[1-9]{1,4}[ \\-]*)|(\\([0-9]{2,3}\\)[ \\-]*)|([0-9]{2,4})[ \\-]*)*?[0-9]{3,4}?[ \\-]*[0-9]{3,4}?$/
-// export const phoneRegExp = /^((\\+[1-9]{1,4}[ \\-]*)|(\\([0-9]{2,3}\\)[ \\-]*)|([0-9]{2,4})[ \\-]*)*?[0-9]{3,4}?[ \\-]*[0-9]{3,4}?$/
 export const onlyLettersDigitsRegExp = /^[а-яА-ЯёЁa-zA-Z0-9]+$/
-// export const onlyLettersRegExp = /^[а-яА-ЯёЁa-zA-Z]+\s+$/
 export const onlyLettersRegExp = /^[а-яА-ЯёЁa-zA-Z]+$/
+
+// export const phoneRegExp = /^\+7((\\+[1-9]{1,4}[ \\-]*)|(\\([0-9]{2,3}\\)[ \\-]*)|([0-9]{2,4})[ \\-]*)*?[0-9]{3,4}?[ \\-]*[0-9]{3,4}?$/
 // export const noSpacesRexExp = /^\S+$/
+// export const onlyLettersDigitsRegExp = /^(?=.*[!@#$%^&(),.+=/\/\]\[{}?><":;|])/
