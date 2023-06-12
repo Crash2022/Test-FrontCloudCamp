52cd3166eb661b49e966af74505d383b384e123b
Crash2022 <crash555@mail.ru>
2023-06-12 18:12:03 +0500

    fix: StepLine styles

--- src/shared/ui/StepLine/StepLine.tsx
+++ src/shared/ui/StepLine/StepLine.tsx
@@ -27,19 +27,19 @@ export const StepLine = ({isSuccess}: StepLineProps) => {
                     <div className={s.step_counter}>
                         { LS_Step1 ? symbol_round : symbol_check }
                     </div>
-                    <div className={s.step_name}>1</div>
+                    <div className={s.step_name_active}>1</div>
                 </div>
                 <div className={LS_Step2 || LS_Step1 ? className_active : className_completed}>
                     <div className={s.step_counter}>
                         { LS_Step2 ? symbol_round : LS_Step3 ? symbol_check : '' }
                     </div>
-                    <div className={s.step_name}>2</div>
+                    <div className={LS_Step2 || LS_Step3 ? s.step_name_active : s.step_name}>2</div>
                 </div>
                 <div className={isSuccess ? className_completed : className_active}>
                     <div className={s.step_counter}>
                         { isSuccess ? symbol_check : LS_Step3 ? symbol_round : '' }
                     </div>
-                    <div className={s.step_name}>3</div>
+                    <div className={LS_Step3 ? s.step_name_active : s.step_name}>3</div>
                 </div>
             </div>
         </>
